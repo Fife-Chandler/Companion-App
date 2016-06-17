8e88fdc0c9d8f40b243f907b6fc96919970d5dff
co075oh <co075oh@W5136980.ena.us.experian.local>
2016-06-17 14:22:02 -0500

    Merge origin/master

---
+++ HomeTeachingCompanion/src/Model/Member.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author co075oh
+ */
+public class Member {
+    
+    // Declare attributes
+    Integer id;
+    String firstName;
+    String middleName;
+    String lastName;
+    String gender;
+    String phone;
+    String dob;
+    String email;
+    boolean hoh;
+    Integer familyID;
+    
+    // Constructor Method
+
+    public Member(Integer id, String firstName, String middleName, String lastName, String gender, String phone, String dob, String email, boolean hoh, Integer familyID) {
+        this.id = id;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.phone = phone;
+        this.dob = dob;
+        this.email = email;
+        this.hoh = hoh;
+        this.familyID = familyID;
+    }
+    
+    // Getter and Setter Methods
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isHoh() {
+        return hoh;
+    }
+
+    public void setHoh(boolean hoh) {
+        this.hoh = hoh;
+    }
+
+    public Integer getFamilyID() {
+        return familyID;
+    }
+
+    public void setFamilyID(Integer familyID) {
+        this.familyID = familyID;
+    }
+    
+}
